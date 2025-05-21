1bd10ad2b4f320e5647df23618be5e2d4651a101
KrupeshVachhani <vachhani.krupesh@gmail.com>
2025-05-21 23:19:28 +0530

    Revert "explore more section in cart page"
    This reverts commit 857929d5452c596ad812f8a0a09e1083d5ca09a0.

--- src/components/body/CartPage/index.jsx
+++ src/components/body/CartPage/index.jsx
@@ -134,20 +134,12 @@ const CartPage = () => {
           <ChevronLeft className="w-5 h-5 mr-1" />
           Back
         </button>
-        <div className="flex justify-between items-center gap-3">
-          <button
-            onClick={() => navigate("/")}
-            className="px-4 py-2 bg-black text-white rounded-lg hover:bg-gray-800 transition-colors hover:cursor-pointer"
-          >
-            Explore More
-          </button>
-          <button
-            onClick={handleEmptyCart}
-            className="flex items-center px-4 py-2 text-red-600 border-2 border-red-600 rounded-lg hover:bg-red-50 transition-colors"
-          >
-            Empty Cart
-          </button>
-        </div>
+        <button
+          onClick={handleEmptyCart}
+          className="flex items-center px-4 py-2 text-red-600 border-2 border-red-600 rounded-lg hover:bg-red-50 transition-colors"
+        >
+          Empty Cart
+        </button>
       </div>
 
       <div className="max-w-4xl mx-auto space-y-8">
